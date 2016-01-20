18fd9faada160a96dc15f4ac4fe9e439d3ddf0a7
MD. Facihul Azam <facihul@yahoo.com>
2016-01-20 07:24:50 +0200

    Delet operation aUpdatedded

--- server.js
+++ server.js
@@ -63,6 +63,36 @@ app.delete('/todos/:id',function (req, res){
 
 });
 
+// Update data
+
+app.put('/todos/:id' , function (req, res){
+
+var todoId = parseInt(req.params.id, 10);
+ var match = _.findWhere(todos, {id: todoId});	
+ var body = _.pick(req.body, 'description', 'completed');
+ var validatt = {};
+  if (!match) {
+  	return res.status(404).send();
+  }
+
+  if (body.hasOwnProperty("completed") && _.isBoolean(body.completed)){
+     validatt.completed = body.completed; 
+  }else if(body.hasOwnProperty('completed')){
+    return res.status(400).send();
+  } 
+
+  if (body.hasOwnProperty('description') &&  _.isString(body.description) && body.description.trim().length > 0) {
+  	validatt.description =  body.description;
+
+  }else if(body.hasOwnProperty('description')){
+  	return res.status(400).send();
+  }
+   
+   _.extend(match, validatt);
+   res.json(match);
+  
+
+}); 
 
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT + '!');
