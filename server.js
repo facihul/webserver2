dfc0b0604591568dbc0118eacb6a63b4648892f8
MD. Facihul Azam <facihul@yahoo.com>
2016-01-22 16:41:10 +0200

    data retrieved into real database by get methode

--- server.js
+++ server.js
@@ -40,25 +40,25 @@ app.get('/todos', function(req, res) {
 app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
 
-  // db.todo.findById(todoId).then(function (todo){
-  //  if(!!todo){
-  //    res.json(todo.toJSON());
-  //  }else{
-  //   res.status(404).send();
-  //  }
-  // },function (e){
-  //   res.status(500).send();
-  // });
-  var match = _.findWhere(todos, {
-    id: todoId
-  });
-
-
-  if (match) {
-    res.json(match);
-  } else {
+  db.todo.findById(todoId).then(function (todo){
+   if(!!todo){
+     res.json(todo.toJSON());
+   }else{
     res.status(404).send();
-  }
+   }
+  },function (e){
+    res.status(500).send();
+  });
+  // var match = _.findWhere(todos, {
+  //   id: todoId
+  // });
+
+
+  // if (match) {
+  //   res.json(match);
+  // } else {
+  //   res.status(404).send();
+  // }
 
 });
 // POST data 
