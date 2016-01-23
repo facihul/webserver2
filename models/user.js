16d5bc483da21e7fcdfceafe83de4e302b63d868
MD. Facihul Azam <facihul@yahoo.com>
2016-01-23 17:45:01 +0200

    users email make unique

--- models/user.js
+++ models/user.js
@@ -1,23 +1,32 @@
-module.exports = function(sequelize, DataTypes){
-return sequelize.define('user', {
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('user', {
 		email: {
-			type : DataTypes.STRING, 
+			type: DataTypes.STRING,
 			allowNull: false,
-			unique : true,
+			unique: true,
 			validate: {
-				isEmail : true
+				isEmail: true
 			}
 		},
 		password: {
 			type: DataTypes.STRING,
-			allowNull : false,
+			allowNull: false,
 			validate: {
-				len: [7,100]
+				len: [7, 100]
 			}
 		}
 
+
+	}, {
+		hooks: {
+			beforeValidate: function(user, option) {
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
+			}
+
+
+		}
 	});
 
-
-
-}
+};
