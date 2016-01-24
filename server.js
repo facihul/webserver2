c4506d94824083448ad5e10cbe176d41dbbc8c95
MD. Facihul Azam <facihul@yahoo.com>
2016-01-24 16:05:19 +0200

    Login authentication is done with post method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var _ = require('underscore');
 var db = require('./db.js')
+var becrypt = require('bcrypt');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todoNestID = 1;
@@ -217,7 +218,7 @@ app.put('/todos/:id', function(req, res) {
 app.post('/user', function(req, res) {
 
   var body = _.pick(req.body, 'email', 'password');
-  db.user.create(body).then(function (user) {
+  db.user.create(body).then(function(user) {
     res.json(user.toPublicJSON());
 
   }, function(e) {
@@ -225,7 +226,16 @@ app.post('/user', function(req, res) {
   });
 });
 
+// psot(/user/login)
+app.post('/user/login', function(req, res) {
+  var body = _.pick(req.body, 'email', 'password');
+  db.user.authenticate(body).then(function (user){
+   res.json(user.toPublicJSON());
+  }, function (){
+   res.status(401).send();
+  });
 
+});
 
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
